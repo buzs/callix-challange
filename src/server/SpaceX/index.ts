db659054c0b556f03f2eaa504585df94b1630598
Gabriel Henrique <me@gabrielhenrique.net>
2023-09-29 04:51:39 -0300

    Add server service base

---
+++ src/server/SpaceX/index.ts
@@ -0,0 +1,21 @@
+export const getUpcomingLaunches = async () => {
+  return {};
+};
+
+export const getPastLaunches = async () => {
+  return {};
+};
+
+export const getNextLaunch = async () => {
+  return {};
+};
+
+export const getLastLaunch = async () => {
+  return {};
+};
+
+export const getLaunchById = async (id: string) => {
+  return {
+    id: id,
+  };
+};
