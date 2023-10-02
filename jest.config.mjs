53cba9173c5bc6b7f03b89e4aa6804bf078b0f64
Gabriel Henrique <me@gabrielhenrique.net>
2023-10-02 18:19:55 -0300

    Fix jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -8,7 +8,7 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testEnvironment: "jest-environment-jsdom",
 };
 
