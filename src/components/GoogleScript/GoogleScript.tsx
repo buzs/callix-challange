b66384eaee972e37e4aa4c14c3886e227352ec16
Gabriel Henrique <me@gabrielhenrique.net>
2023-10-02 23:32:47 -0300

    remove process env into scripts

--- src/components/GoogleScript/GoogleScript.tsx
+++ src/components/GoogleScript/GoogleScript.tsx
@@ -6,7 +6,7 @@ export default function GoogleScript() {
   return (
     <>
       <Script
-        src={`https://www.googleoptimize.com/optimize.js?id=${process.env.GOOGLE_OPTIMIZE_ID}`}
+        src={`https://www.googleoptimize.com/optimize.js?id=GTM-MB39XR7`}
       />
       <Script
         id="gtm"
@@ -15,7 +15,7 @@ export default function GoogleScript() {
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER_ID}');`,
+        })(window,document,'script','dataLayer','GTM-5RWDDMQD');`,
         }}
       />
     </>
--- src/components/HotjarScript/HotjarScript.tsx
+++ src/components/HotjarScript/HotjarScript.tsx
@@ -9,7 +9,7 @@ export default function HotjarScript() {
       dangerouslySetInnerHTML={{
         __html: `(function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:${process.env.HOTJAR_SITE_ID},hjsv:${process.env.HOTJAR_VERSION}};
+            h._hjSettings={hjid:3676124,hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
