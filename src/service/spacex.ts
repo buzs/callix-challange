3c35f14e9440c9c595a0f20fa48d1839ae097dc1
Gabriel Henrique <me@gabrielhenrique.net>
2023-10-03 13:36:12 -0300

    Fix "dynamic-server-error" and improve error msg

--- src/service/spacex.ts
+++ src/service/spacex.ts
@@ -2,27 +2,23 @@ import { headers } from "next/headers";
 import { Launch, Launches } from "@/server/SpaceX/types";
 
 async function fetchRequest<T>(path: string) {
+  const host = headers().get("host");
   try {
-    const host = headers().get("host");
     const protocol = process?.env.NODE_ENV === "development" ? "http" : "https";
 
     const URL = process.env.INTERNAL_API_URL || `${protocol}://${host}/api/`;
 
     const response = await fetch(URL + path, { next: { revalidate: 3600 } });
 
-    //   const rPath = await import(`../app/api/${path}/route`);
-    //   const response = await rPath.GET();
-
     if (!response.ok) {
-      console.warn(
-        process.env.INTERNAL_API_URL || `${protocol}://${host}/api/` + path
-      );
-      throw new Error("Failed to fetch data");
+      throw new Error(`Failed to fetch data in "${URL + path}"`);
     }
 
     return response.json() as Promise<T>;
   } catch (error) {
-    console.error(path, error);
+    if (error instanceof Error) {
+      console.error(`[${path}]`, error.message);
+    }
   }
 }
 
