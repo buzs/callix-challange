f12b767fe6ba0a919a141ceb8637fad91c42cafa
Gabriel Henrique <me@gabrielhenrique.net>
2023-10-02 23:45:42 -0300

    Add google analytics in GoogleScript

--- src/components/GoogleScript/GoogleScript.tsx
+++ src/components/GoogleScript/GoogleScript.tsx
@@ -9,13 +9,18 @@ export default function GoogleScript() {
         src={`https://www.googleoptimize.com/optimize.js?id=GTM-MB39XR7`}
       />
       <Script
-        id="gtm"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-JRCF93LFQ7`}
+      />
+      <Script
+        id="google-analytics"
         dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5RWDDMQD');`,
+          __html: `
+					window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-JRCF93LFQ7');
+					`,
         }}
       />
     </>
