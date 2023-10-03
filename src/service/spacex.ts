5eb56a38ccbfbb4e023094ff2740ca71b8a8d12d
Gabriel Henrique <me@gabrielhenrique.net>
2023-10-02 22:19:27 -0300

    Fix get host in client service

--- src/service/spacex.ts
+++ src/service/spacex.ts
@@ -1,41 +1,47 @@
-import { Launch, Launches } from "@/server/SpaceX/types"
+import { headers } from "next/headers";
+import { Launch, Launches } from "@/server/SpaceX/types";
 
 async function fetchRequest<T>(path: string) {
-    const response = await fetch(process.env.INTERNAL_API_URL + path, { next: { revalidate: 3600 } })
+  const host = headers().get("host");
+  const protocol = process?.env.NODE_ENV === "development" ? "http" : "https";
+  const response = await fetch(
+    process.env.INTERNAL_API_URL || `${protocol}://${host}/api/` + path,
+    { next: { revalidate: 3600 } }
+  );
 
-    if (!response.ok) {
-        throw new Error('Failed to fetch data')
-    }
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-    return response.json() as Promise<T>
+  return response.json() as Promise<T>;
 }
 
 export const getUpcomingLaunches = async () => {
-    const data = await fetchRequest<Launches>(`launches/upcoming`)
+  const data = await fetchRequest<Launches>(`launches/upcoming`);
 
-    return data
-}
+  return data;
+};
 
 export const getPastLaunches = async () => {
-    const data = await fetchRequest<Launches>(`launches/past`)
+  const data = await fetchRequest<Launches>(`launches/past`);
 
-    return data
-}
+  return data;
+};
 
 export const getNextLaunch = async () => {
-    const data = await fetchRequest<Launch>(`launches/next`)
+  const data = await fetchRequest<Launch>(`launches/next`);
 
-    return data
-}
+  return data;
+};
 
 export const getLastLaunch = async () => {
-    const data = await fetchRequest<Launch>('launches/latest')
+  const data = await fetchRequest<Launch>("launches/latest");
 
-    return data
-}
+  return data;
+};
 
 export const getLaunchById = async (id: string) => {
-    const data = await fetchRequest<Launch>(`launches/${id}`)
+  const data = await fetchRequest<Launch>(`launches/${id}`);
 
-    return data
-}
+  return data;
+};
